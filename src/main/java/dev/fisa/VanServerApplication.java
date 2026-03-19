e26a08a73fee2b31ab942fb92335b9714a55d4b4
aransword <lupin521@gmail.com>
2026-03-19 17:04:53 +0900

    first setting

---
+++ src/main/java/dev/fisa/VanServerApplication.java
@@ -0,0 +1,13 @@
+package dev.fisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanServerApplication.class, args);
+	}
+
+}
