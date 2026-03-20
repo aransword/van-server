3907dd453bd26654a1e5ce1462d7b1c790af513e
seonjiwon <tommys915@gmail.com>
2026-03-20 09:40:07 +0900

    feat: restClient

---
+++ src/main/java/dev/fisa/domain/payment/dto/VanResponse.java
@@ -0,0 +1,10 @@
+package dev.fisa.domain.payment.dto;
+
+import lombok.Builder;
+
+@Builder
+public record VanResponse(
+
+) {
+
+}
---
+++ src/main/java/dev/fisa/domain/payment/service/CardClientService.java
@@ -0,0 +1,13 @@
+package dev.fisa.domain.payment.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CardClientService {
+
+}
